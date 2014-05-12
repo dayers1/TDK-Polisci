82ed554ce1f663e1a58a4440d99ce83f519ba057
viracerbus <kevin.dexter@comcast.net>
2014-05-12 13:33:41 -0500

    Update clientModelServiceAsync.java

--- ThesisDB/src/edu/gac/polisci/client/clientModelServiceAsync.java
+++ ThesisDB/src/edu/gac/polisci/client/clientModelServiceAsync.java
@@ -1,5 +1,7 @@
 package edu.gac.polisci.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import edu.gac.polisci.shared.Thesis;
@@ -9,5 +11,6 @@ public interface clientModelServiceAsync {
 	void setAppBaseURL(String homeURL, AsyncCallback<String> asyncCallback);
 	void isUserLoggedIn(AsyncCallback<Boolean> asyncCallback);
 	void submitThesisToServer(Thesis thesis, AsyncCallback<String> callback);
+	void getThesesFromServer(AsyncCallback<List<Thesis>> callback);
 
 }
